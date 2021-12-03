e23017d13bde0ee21ff1f3bd57bb3472d38b65c5
shaidakoder <62111555+shaidakoder@users.noreply.github.com>
2021-12-03 17:55:59 +0200

    need to fix arrays

---
+++ ex1/wet/implementation/amount_set_str.c
@@ -0,0 +1,58 @@
+#include "amount_set_str.h"
+#include <stdio.h>
+#include <assert.h>
+
+#include <malloc.h>
+//set is an array
+
+struct AmountSet_t {
+    int size;
+    char** elements;
+    int* amount_array;
+    int iterator;
+};
+AmountSet asCreate(){
+    AmountSet set = malloc(sizeof (*set));
+    if(set == NULL){
+        return NULL;
+    }
+    set->elements = NULL;
+    set->amount_array = NULL;
+    //in add we will check if size =0 and set elements and amount array
+    set->iterator = 0;
+    set->size = 0;
+    return set;
+}
+
+void asDestroy(AmountSet set) {
+    if (set == NULL)
+        return;
+    double *outAmount;
+    while (asGetAmount(set, set->elements, outAmount)) {
+        asDelete(set, asGetFirst(set));
+    }
+    free(set->elements);
+    free(set->amount_array);
+    free(set);
+}
+AmountSet asCopy(AmountSet set){//iterator isn't copied
+    if(set == NULL)
+        return -1;
+    AmountSet ans = asCreate();
+    if(set->size == 0){
+        return  ans;
+    }
+    //maybe iterator should be copied here
+    ans->size = set->size;
+    ans->elements = malloc(*(ans->elements))
+    for (int i = 0; i < set->size; ++i) {
+
+    }
+
+}
+
+//for quick tests
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
