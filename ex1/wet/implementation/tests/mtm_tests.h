fad797a924751ccb1575e65720c357d76bd4b9c9
IntelliFinder <snirhordan@gmail.com>
2021-12-04 14:18:23 +0200

    tests-separate and orgainzed just beginning

---
+++ ex1/wet/implementation/tests/mtm_tests.h
@@ -0,0 +1,14 @@
+#ifndef MTM_TESTS_H_
+#define MTM_TESTS_H_
+
+#include <stdbool.h>
+
+bool testCreate();
+bool testDestroy();
+bool testModifyProducts();
+bool testModifyOrders();
+bool testPrintInventory();
+bool testPrintOrder();
+bool testPrintBestSelling();
+
+#endif /* MTM_TESTS_H_ */
