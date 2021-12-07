0720a8eebb0a4399127ace5d9716d375e9e36c54
IntelliFinder <snirhordan@gmail.com>
2021-12-07 20:59:24 +0200

    Update matamikya.c

--- ex1/wet/implementation/matamikya.c
+++ ex1/wet/implementation/matamikya.c
@@ -22,11 +22,18 @@ typedef void MtmProductData_t{
 
 } *MtmProductData;
 
+typedef void Order_t{
+    unsigned int id;
+    Set order;
+}*Order;
+
 SetElement copyMtmProductData( SetElement set_element );
 void freeMtmProductData( SetElement set_element );
 int compareMtmProductData( SetElement set_element_1,SetElement set_element_2 );
 
-
+void FreeListElement( SetElement setElement );
+SetElement copySetElements( SetElement SetElement );
+int compareSetElements( SetElement setElement, SetElement setElement );
 
 
 int main() {
@@ -71,4 +78,49 @@ int compareMtmProductData( SetElement set_element_1, SetElement set_element_2 )
         return -1;
     }
     return 0;
+}
+
+MtmProductData MtmCopyData( MtmProductData product )
+{
+    if( product == NULL ){
+        return NULL;
+    }
+    char* name = malloc(strlen(*product->units) + 1);
+    if( name ==NULL)
+    {
+        return NULL;
+    }
+    *name = *(product->units);
+    MtmProductData new = { product->id, product->amount, name, product->amountType };
+    return new;
+
+}
+MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const char *name,
+                              const double amount, const MatamikyaAmountType amountType,
+                              const MtmProductData customData, MtmCopyData copyData,
+                              MtmFreeData freeData, MtmGetProductPrice prodPrice)
+{ //matamikya non-null
+    if( matamikya == NULL || name==NULL || amountType ==NULL ||
+                             customData ==NULL || freeData ==NULL || prodPrice ==NULL )
+    {
+        return MATAMIKYA_NULL_ARGUMENT;
+    }
+    if( !(65<=(int)(*name) <=90 || 97<=(int)(*name)<=122 || 0<=(int)(*name)<=9)){
+        return MATAMIKYA_INVALID_NAME;
+    }
+    assert( (65<=(int)(*name) <=90 || 97<=(int)(*name)<=122 || 0<=(int)(*name)<=9) );
+    if ( amount<0 || !(amountType == MATAMIKYA_INTEGER_AMOUNT || (amountType == MATAMIKYA_ANY_AMOUNT|| (amountType == MATAMIKYA_HALF_INTEGER_AMOUNT )){
+        return MATAMIKYA_INVALID_AMOUNT;
+    }
+    //go over all elements in set check id take maximum and add one 
+    if( setIsIn(matamikya->mtm, customData) ){
+        return MATAMIKYA_PRODUCT_ALREADY_EXIST;
+    }
+    customData->name = name;
+    customData->id =id;
+    customData->amount = amount;
+    
+    MtmProductData product = MtmCopyData( customData ); // why do we need more details other than customData?
+
+    return MATAMIKYA_SUCCESS;
 }
