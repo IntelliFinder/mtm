50c6f545c83b88eabb8c3318c7f461e888b55eac
IntelliFinder <snirhordan@gmail.com>
2021-12-13 18:49:48 +0200

    jetbrain

---
+++ ex1/wet/jetbrain/tests/amount_set_str_main.c
@@ -0,0 +1,14 @@
+#include "amount_set_str_tests.h"
+#include "amount_set_str_test_utilities.h"
+
+int main()
+{
+    RUN_TEST(testCreate);
+    RUN_TEST(testDestroy);
+    RUN_TEST(testModifyProducts);
+    RUN_TEST(testModifyOrders);
+    RUN_TEST(testPrintInventory);
+    RUN_TEST(testPrintOrder);
+    RUN_TEST(testPrintBestSelling);
+    return 0;
+}
