8b6c1e975f4c0b8c7ef18a48a4c90c5eaa06aceb
IntelliFinder <snirhordan@gmail.com>
2022-01-13 18:21:11 +0200

    namespace

--- ex2/zipping folder/solution/Citizen.cpp
+++ ex2/zipping folder/solution/Citizen.cpp
@@ -5,47 +5,47 @@
 
 
 #include "Citizen.h"
-using namespace mtm;
+namespace mtm{
 
-int Citizen::getId() const { return id; }
-std::string Citizen::getFirstName() const {
-    return firstName;
-}
-std::string Citizen::getLastName() const {
-    return lastName;
-}
-int Citizen::getBirthYear() const {
-    return birthYear;
-}
-
-void Citizen::printShort( std::ostream &str) {
-    str<<std::endl; //placeholder
-}
-void Citizen::printLong( std::ostream &str ){
-    str<<std::endl; //placeholder
-}
-namespace mtm {
-    bool operator==(Citizen &c1, Citizen &c2) {
-        return c1.getId() == c2.getId();
+    int Citizen::getId() const { return id; }
+    std::string Citizen::getFirstName() const {
+        return firstName;
+    }
+    std::string Citizen::getLastName() const {
+        return lastName;
+    }
+    int Citizen::getBirthYear() const {
+        return birthYear;
     }
 
-    bool operator<=(Citizen &c1, Citizen &c2) {
-        return c1.getId() <= c2.getId();
+    void Citizen::printShort( std::ostream &str) {
+        str<<std::endl; //placeholder
     }
+    void Citizen::printLong( std::ostream &str ){
+        str<<std::endl; //placeholder
+    }
+   bool operator==(Citizen &c1, Citizen &c2) {
+       return c1.getId() == c2.getId();
+   }
 
-    bool operator>=(Citizen &c1, Citizen &c2) {
-        return c1.getId() >= c2.getId();
-    }
+   bool operator<=(Citizen &c1, Citizen &c2) {
+       return c1.getId() <= c2.getId();
+   }
 
-    bool operator!=(Citizen &c1, Citizen &c2) {
-        return c1.getId() != c2.getId();
-    }
+   bool operator>=(Citizen &c1, Citizen &c2) {
+       return c1.getId() >= c2.getId();
+   }
 
-    bool operator<(Citizen &c1, Citizen &c2) {
-        return c1.getId() < c2.getId();
-    }
+   bool operator!=(Citizen &c1, Citizen &c2) {
+       return c1.getId() != c2.getId();
+   }
 
-    bool operator>(Citizen &c1, Citizen &c2) {
-        return c1.getId() > c2.getId();
-    }
+   bool operator<(Citizen &c1, Citizen &c2) {
+       return c1.getId() < c2.getId();
+   }
+
+   bool operator>(Citizen &c1, Citizen &c2) {
+       return c1.getId() > c2.getId();
+   }
+
 }
--- ex2/zipping folder/solution/Employee.cpp
+++ ex2/zipping folder/solution/Employee.cpp
@@ -5,90 +5,95 @@
 
 #include "Employee.h"
 
-using namespace mtm;
+namespace mtm{
 
-int Employee::getSalary() const{
-    return salary;
-}
+    int Employee::getSalary() const{
+        return salary;
+    }
 
-int Employee::getScore() const{
-    return score;
-}
+    int Employee::getScore() const{
+        return score;
+    }
 
-bool Employee::hasSkill(const int skillId)  const {
-    for(std::set<Skill>::iterator i = skillSet.begin() ; i != skillSet.end(); ++i) {
-        if (i->getId() == skillId){
-            return true;
+    bool Employee::hasSkill(const int skillId)  const {
+        for(std::set<Skill>::iterator i = skillSet.begin() ; i != skillSet.end(); ++i) {
+            if (i->getId() == skillId){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    void Employee::learnSkill(Skill const& skill) {
+        if(hasSkill(skill.getId())){
+            throw mtm::SkillAlreadyLearned();
+        }
+        if (score<skill.getRequiredPoints()){
+            throw CanNotLearnSkill();
+        }
+
+        skillSet.insert(skill);
+    }
+
+    void Employee::forgetSkill(const int skillId) {
+        for ( std::set<Skill>::iterator i = skillSet.begin(); i!=skillSet.end(); ++i ) {
+            if (i->getId() == skillId){
+                skillSet.erase(i);
+                return;
+            }
+        }
+        throw DidNotLearnSkill();
+    }
+
+    std::set<Skill> Employee::getCopySkillSet() const{
+        std::set<Skill> temp = this->skillSet; //alllocates new memory and copies set
+        return temp;
+    }
+
+
+    void helpForSetInt(int &toChange, const int toAdd){
+        toChange += toAdd;
+        if (toChange < 0){
+            toChange = 0;
         }
     }
-    return false;
-}
-
-
-void Employee::learnSkill(Skill const& skill) {
-    if(hasSkill(skill.getId())){
-        throw mtm::SkillAlreadyLearned();
-    }
-    if (score<skill.getRequiredPoints()){
-        throw CanNotLearnSkill();
+    void Employee::setSalary(const int addSalary) {
+        helpForSetInt(salary,addSalary);//here the func is ok
     }
 
-    skillSet.insert(skill);
-}
+    void Employee::setScore(const int addScore) {
+        helpForSetInt(score,addScore); //look! course didn't say what to do in case of score < 0 , snir: good point
+    }
 
-void Employee::forgetSkill(const int skillId) {
-    for ( std::set<Skill>::iterator i = skillSet.begin(); i!=skillSet.end(); ++i ) {
-        if (i->getId() == skillId){
-            skillSet.erase(i);
-            return;
+    void Employee::printShort(std::ostream &os){
+        os <<getFirstName() + " " + getLastName()<<std::endl;
+        std::string salaryStr = std::to_string(salary);
+        std::string scoreStr = std::to_string(score);
+        os << "Salary: " + salaryStr + " Score: " + scoreStr << std::endl;
+    }
+
+    void Employee::printLong(std::ostream& os) {
+        std::string idStr = std::to_string(getId());
+        std::string birthYearStr = std::to_string(getBirthYear());
+        std::string salaryStr = std::to_string(salary);
+        std::string scoreStr = std::to_string(score);
+        os << getFirstName() + " " + getLastName()<<std::endl;
+        os << "id - " + idStr + " birth_year - " + birthYearStr<<std::endl;
+        os << "Salary: " + salaryStr + " Score: " + scoreStr ;
+        if(skillSet.size()!=0){
+            os<<" Skills: "<< std::endl;
+            for (std::set<Skill>::iterator i = skillSet.begin(); i!=skillSet.end(); ++i) {
+                os << i->getName() << std::endl; //ERROR overloading doesn't work
+            }
         }
-    }
-    throw DidNotLearnSkill();
-}
-
-std::set<Skill> Employee::getCopySkillSet() const{
-    std::set<Skill> temp = this->skillSet; //alllocates new memory and copies set
-    return temp;
-}
-
-
-void helpForSetInt(int &toChange, const int toAdd){
-    toChange += toAdd;
-    if (toChange < 0){
-        toChange = 0;
-    }
-}
-void Employee::setSalary(const int addSalary) {
-    helpForSetInt(salary,addSalary);//here the func is ok
-}
-
-void Employee::setScore(const int addScore) {
-    helpForSetInt(score,addScore); //look! course didn't say what to do in case of score < 0 , snir: good point
-}
-
-void Employee::printShort(std::ostream &os){
-    os <<getFirstName() + " " + getLastName()<<std::endl;
-    std::string salaryStr = std::to_string(salary);
-    std::string scoreStr = std::to_string(score);
-    os << "Salary: " + salaryStr + " Score: " + scoreStr << std::endl;
-}
-
-void Employee::printLong(std::ostream& os) {
-    std::string idStr = std::to_string(getId());
-    std::string birthYearStr = std::to_string(getBirthYear());
-    std::string salaryStr = std::to_string(salary);
-    std::string scoreStr = std::to_string(score);
-    os << getFirstName() + " " + getLastName()<<std::endl;
-    os << "id - " + idStr + " birth_year - " + birthYearStr<<std::endl;
-    os << "Salary: " + salaryStr + " Score: " + scoreStr ;
-    if(skillSet.size()!=0){
-        os<<" Skills: "<< std::endl;
-        for (std::set<Skill>::iterator i = skillSet.begin(); i!=skillSet.end(); ++i) {
-            os << i->getName() << std::endl; //ERROR overloading doesn't work
+        else{
+            os<<std::endl;
         }
+
     }
-    else{
-        os<<std::endl;
-    }
+
+
+
 
 }
