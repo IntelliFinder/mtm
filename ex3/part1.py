3ad0318ff73f58bcdfa15329b5769b0896be26b2
IntelliFinder <snirhordan@gmail.com>
2022-01-20 00:52:53 +0200

    began code using list copmrehensions, best array for keeping track of amount sold, updates amount changes(no tests), now need to ship order and count sales

--- ex3/part1.py
+++ ex3/part1.py
@@ -1,13 +1,19 @@
 def find_best_selling_product(filename):
-    sequence = [line.split().extend([0]) for line in filename]
+    sequence = [line.split() for line in filename]
+    best = []
+    for item in sequence:
+        if item[0] == 'add' and item[1] == 'product':
+            best.append([item[2], float(item[3]), float(item[4]), 0])
+        if item[0] == 'change' and item[1] == 'amount':
+            best[[idx for idx, val in enumerate(best) if val[0] == item[2]][0]][2] += float(item[3])
     return None
 
 
 def main():
     file = open("example.txt", 'r')
-
     find_best_selling_product(file)
     file.close()
+    print("Hello World!")
     return 0
 
 
