b7f5f1b734a9de109ac8d117321602aa36126c2b
IntelliFinder <snirhordan@gmail.com>
2022-01-20 17:10:44 +0200

    update

--- ex3/part1.py
+++ ex3/part1.py
@@ -7,7 +7,11 @@ def find_best_selling_product(filename):
         if item[0] == 'change' and item[1] == 'amount':
             best[item[2]][1] += float(item[3])
         if item[0] == 'ship' and item[1] == 'order':
-            orders = [line.split('--') for line in filename if line.split()[0] == 'ship' and line.split()[1] == 'order']
+            orders = [[item[i+1][:-1], float(item[i+2])] for i, add in enumerate(item) if add == 'order' or add == '--']
+            for order in orders:
+                if (best[order[0]][1] - order[1]) >= 0 and (order[0] in [key for key in best.keys()]):
+                    best[order[0]][1] -= order[1]
+                    best[order[0]][2] += order[1]*best[order[0]][0]
     print(best)
     return None
 
