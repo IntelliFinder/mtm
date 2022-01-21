ecdd0428d500be4d433f72349c5ee43736d3e94e
IntelliFinder <snirhordan@gmail.com>
2022-01-21 16:55:37 +0200

    update code, change list to dict, total lines = 27<30(including return and headline),all tests passed!

--- ex3/hw3_tests (20,1 time 11,51)/hw3_part1.py
+++ ex3/hw3_tests (20,1 time 11,51)/hw3_part1.py
@@ -9,39 +9,28 @@ def find_best_selling_product(filename):
     sequence = [line.split() for line in file]
     file.close()
     if not sequence:
-        value = ('', 0)
-        return value
+        return ('', 0)
     best = {}
-    #debug_orders = []
     for idx, item in enumerate(sequence):
         if item[0] == 'add' and item[1] == 'product':
             if item[2] not in best:
-                best[item[2]] = [float(item[3]), float(item[4]), 0] #maybe better dict of dicts but whateverS
-            #print(best)
+                best[item[2]] = {'price': float(item[3]), 'amount': float(item[4]), 'total': 0}
         if item[0] == 'change' and item[1] == 'amount':
             if item[2] in best:
-                best[item[2]][1] += float(item[3])
-            #print(best)
+                best[item[2]]['amount'] += float(item[3])
         if item[0] == 'ship' and item[1] == 'order':
-            orders = [[item[i+1][:-1], float(item[i+2])] for i, add in enumerate(item) if add == 'order' or add == '--']
+            orders = [[item[i+1][:-1], float(item[i+2])] for i, j in enumerate(item) if j == 'order' or j == '--']#not dict because can be duplication
             for order in orders:
                 name = order[0]
-                amount = 1
-                price = 2
                 if name in best:
-                    if (best[name][amount] - order[amount]) >= 0:
-                        ###debug_orders.append([name, order[1], order[1]*best[name][0]])
-                        best[name][amount] -= float(order[amount])
-                        best[name][price] += float(order[amount]*best[name][0])# price times order amount
-            #print(best)
-    #get maximum sold product and total money made while shipping it
-    current_selling_price = max([val[2] for val in best.values()])
-    products = sorted([key for key in best.keys() if best[key][2] == current_selling_price])
+                    if (best[name]["amount"] - order[1]) >= 0:
+                        best[name]["amount"] -= float(order[1])
+                        best[name]["total"] += float(order[1]*best[name]["price"])# price times order amount
+    current_selling_price = max([val["total"] for val in best.values()])
+    products = sorted([key for key in best.keys() if best[key]["total"] == current_selling_price])
     if not products:
-        value = ('', 0)
-        return value
-    value = (products[0], current_selling_price)
-    return value
+        return ('', 0)
+    return (products[0], current_selling_price)
 
 
 def main():
